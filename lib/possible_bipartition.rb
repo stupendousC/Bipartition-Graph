231807f7f33a870638d9a9bfb459e4bfa4d750c3
stupendousC <CarolineWuKaplan@gmail.com>
2020-04-02 23:13:28 -0700

    initial commit, just playing here... will prob change later

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,31 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  
+  visited = {}
+  # visited will have key of index# for each dog, and value of T/F for easy flipping back and forth
+  
+  dislikes.each do |i|
+    if dislikes[i] == []
+      # friendly dog, can go anywhere
+      if !visited[i]
+        visited[i] = true
+      end
+        
+    elsif visited[i].nil?
+      queue = [i]
+      visited[i] = true
+
+      until queue.empty?
+        currDog = queue.pop()
+
+        dislikes[currDog].each do |enemy|
+          queue << enemy
+          visited[enemy] = !visited[i]
+        end
+
+      end
+    end
+    
+  end
+
 end
